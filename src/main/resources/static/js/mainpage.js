1c5dacd6e7dbcdc8760eb4f9a99275f40777b880
Tianle Cai <tcai38@gatech.edu>
2023-02-08 16:01:34 -0500

    Organize js files

---
+++ src/main/resources/static/js/mainpage.js
@@ -0,0 +1,13 @@
+function goToUploadPage() {
+    window.location.href="traitspage.html";
+}
+
+function loadCSV() {
+    var fileToLoad = document.getElementById("fileToLoad").files[0];
+    //TODO: feed to backend
+
+
+    //https://stackoverflow.com/questions/31746837/reading-uploaded-text-file-contents-in-html
+
+}
+
--- src/main/resources/static/js/traitspage.js
+++ src/main/resources/static/js/traitspage.js
@@ -7,22 +7,9 @@ function TA(firstName, lastName, yearsExperience, hasCar) {
     this.hasCar = hasCar;
 }
 
-function goToUploadPage() {
-    window.location.href="traitspage.html";
-}
 function goToMainPage() {
     window.location.href="mainpage.html";
 }
-function loadCSV() {
-    var fileToLoad = document.getElementById("fileToLoad").files[0];
-    //TODO: feed to backend
-
-
-    //https://stackoverflow.com/questions/31746837/reading-uploaded-text-file-contents-in-html
-
-}
-
-
 
 function getLength() {
     alert(tas.length);
