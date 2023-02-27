83487a442c0e62b9b55bde4dccc100b7e0ec3276
pkutz3 <pkutz3@gatech.edu>
2023-02-27 02:25:58 -0500

    adding filter by name

--- src/main/resources/static/js/csv_table.js
+++ src/main/resources/static/js/csv_table.js
@@ -117,6 +117,27 @@ function sortTableByYear() {
     }
 }
 
+function findName() {
+    var input, filter, table, tr, td, i, txtValue;
+    input = document.getElementById("nameInput");
+    filter = input.value.toUpperCase();
+    table = document.getElementById("table");
+    tr = table.getElementsByTagName("tr");
+
+  // Loop through all table rows, and hide those who don't match the search query
+    for (i = 0; i < tr.length; i++) {
+        td = tr[i].getElementsByTagName("td")[0];
+        if (td) {
+            txtValue = td.textContent || td.innerText;
+            if (txtValue.toUpperCase().indexOf(filter) > -1) {
+                tr[i].style.display = "";
+            } else {
+        tr[i].style.display = "none";
+            }
+        }
+    }
+}
+
 function init_modal() {
     // Get the modal
     var modal = document.getElementById("myModal");
