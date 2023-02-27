ce773b32fbbbac58599ecd9d1e26c73c8e074c79
pkutz3 <pkutz3@gatech.edu>
2023-02-27 02:41:37 -0500

    adding sort by year

--- src/main/resources/static/js/csv_table.js
+++ src/main/resources/static/js/csv_table.js
@@ -118,18 +118,41 @@ function sortTableByYear() {
 }
 
 function findName() {
-    var input, filter, table, tr, td, i, txtValue;
+    var input, filter, table, tr, td, i, text;
+    // get name input from type box
     input = document.getElementById("nameInput");
     filter = input.value.toUpperCase();
     table = document.getElementById("table");
     tr = table.getElementsByTagName("tr");
 
-  // Loop through all table rows, and hide those who don't match the search query
+    // loop through table and keep only matches
     for (i = 0; i < tr.length; i++) {
         td = tr[i].getElementsByTagName("td")[0];
         if (td) {
-            txtValue = td.textContent || td.innerText;
-            if (txtValue.toUpperCase().indexOf(filter) > -1) {
+            text = td.textContent || td.innerText;
+            if (text.toUpperCase().indexOf(filter) > -1) {
+                tr[i].style.display = "";
+            } else {
+        tr[i].style.display = "none";
+            }
+        }
+    }
+}
+
+function findYear() {
+    var input, filter, table, tr, td, i, text;
+    // get name input from type box
+    input = document.getElementById("yearInput");
+    filter = input.value.toUpperCase();
+    table = document.getElementById("table");
+    tr = table.getElementsByTagName("tr");
+
+    // loop through table and keep only matches
+    for (i = 0; i < tr.length; i++) {
+        td = tr[i].getElementsByTagName("td")[1];
+        if (td) {
+            text = td.textContent || td.innerText;
+            if (text.toUpperCase().indexOf(filter) > -1) {
                 tr[i].style.display = "";
             } else {
         tr[i].style.display = "none";
